66e4e6479601f09930befec5028547142c820e5b
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-10 10:25:32 +0530

    Dependency Injection - Injected UserManager & SignInManager to ApplicationUserController

--- GuideU Web App/Controllers/ApplicationUserController.cs
+++ GuideU Web App/Controllers/ApplicationUserController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using GuideU_Web_App.Models;
 
 namespace GuideU_Web_App.Controllers
 {
@@ -11,5 +13,15 @@ namespace GuideU_Web_App.Controllers
     [ApiController]
     public class ApplicationUserController : ControllerBase
     {
+        // we need these for user registration and authentication
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        //inject these classes to this controller constructor
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
     }
 }
