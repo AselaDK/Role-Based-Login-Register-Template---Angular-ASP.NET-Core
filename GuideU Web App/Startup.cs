248477c3ee0cbfbbfca24d6ce70d717bf5fac979
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-10 11:20:03 +0530

    Changed some validations of Identity User

--- GuideU Web App/Startup.cs
+++ GuideU Web App/Startup.cs
@@ -39,6 +39,14 @@ namespace GuideU_Web_App
             // ApplicationUserControl will dependency injecthere
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AuthenticationContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
