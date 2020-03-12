a37377e5d56be4078541a171a0a2353f2cac3cc3
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-12 10:56:51 +0530

    form validatons are done

--- guideufend/src/app/services/user.service.ts
+++ guideufend/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,23 @@ export class UserService {
   constructor(private fb:FormBuilder) { }
 
   formModel = this.fb.group({
-    UserName: [''],
-    Email: [''],
-    FullName: [''],
+    UserName: ["", Validators.required],
+    Email: ["", [Validators.required, Validators.email]],
+    FullName: ["", Validators.required],
     Passwords: this.fb.group({
-      Password: [''],
-      ConfirmPassword: ['']
-    })
+      Password: ["", [Validators.required, Validators.minLength(6)]],
+      ConfirmPassword: ["", Validators.required]
+    }, {validator: this.comparePasswords})
   });
+
+  comparePasswords(fb:FormGroup){
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //password mismatch
+    if(confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors){
+      if(fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
 }
