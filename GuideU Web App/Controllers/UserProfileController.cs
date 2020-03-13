12f308b22d46f52f75d2e95976cdeb9bb6c7f350
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-13 23:00:31 +0530

    Login API is DONE!

---
+++ GuideU Web App/Controllers/UserProfileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuideU_Web_App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// for secure routes
+
+namespace GuideU_Web_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        //method to get email, fullname etc..
+
+        [HttpGet]
+        [Authorize]
+        //GET: /api/UserProfile
+        public async Task<Object> GetUserProfile()
+        {
+            // has to get the jwt token to identify whish user is making the request
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;   // "UserId" is the property that Claimed in ApplicationUserController
+            var user = await _userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+
+    }
+}
