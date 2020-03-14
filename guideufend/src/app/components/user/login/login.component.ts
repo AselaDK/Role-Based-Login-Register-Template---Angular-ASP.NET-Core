3e4874081a570daa8f6ad20ea7ac1ccc594bf07b
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-14 10:30:13 +0530

    prevented routing loginpage while already logged in

--- guideufend/src/app/components/user/login/login.component.ts
+++ guideufend/src/app/components/user/login/login.component.ts
@@ -20,6 +20,8 @@ export class LoginComponent implements OnInit {
   constructor(private service:UserService, private router:Router, private toastr:ToastrService) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('token')!=null)
+      this.router.navigateByUrl('/home');
   }
 
   onSubmit(form: NgForm){
